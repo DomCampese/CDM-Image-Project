72a43ec726405470bfb1a9af41e3586c92ee255c
Dominic Campese <62118391+DomCampese@users.noreply.github.com>
2021-09-21 00:00:39 -0400

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+let blurredImg;
+let originalImg;
+
+function preload() {
+    originalImg = loadImage('images/test.jpg');
+    blurredImg = loadImage('images/test.jpg');
+}
+
+function setup() {
+    const w = windowWidth;
+    const h = windowHeight;
+    createCanvas(w, h);
+    pixelDensity(1);
+    blurredImg.filter(GRAY);
+    blurredImg.filter(BLUR, 4); /* Add the blur to one image */
+    blurredImg.loadPixels();
+}
+
+function draw() {
+    /* Set pixels minDistance pixels or less away from cursor
+       to transparent to reveal sharp image behind */
+    const minDistance = 150
+    for (let x = 0; x < blurredImg.width; x++) {
+        for (let y = 0; y < blurredImg.height; y++) {
+            let pixelLoc = (x + y * blurredImg.width) * 4; // 2d to 1d location
+            let pixelDistanceFromMouse = dist(x, y, mouseX, mouseY);
+            /* Results in a circle with radius minDistance */
+            if (pixelDistanceFromMouse <= minDistance) {
+                blurredImg.pixels[pixelLoc + 3] = 0; // Pixel stored as [r, g, b, a] (a/alpha is transparency)
+             } else {
+                // Set opaque
+                blurredImg.pixels[pixelLoc + 3] = 255;
+            }
+        }
+    }
+    blurredImg.updatePixels();
+    image(originalImg, 0, 0);
+    image(blurredImg, 0, 0); 
+    
+}
